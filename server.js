5eed937c54944214a587d3ab307ea029adb9e44b
NotaSurgele <alexis.picard@epitech.eu>
2021-07-21 20:21:48 +0400

    detect if client want to create or join a room

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 5432;
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
@@ -10,8 +12,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-    console.log(__dirname);
     res.sendFile('public/home.html', {root: __dirname});
+});
+
+app.post('/checking', (req, res) => {
+    if (req.body.room == "Create Room !") {
+        res.redirect('/create');
+    } else {
+        res.redirect('/join');
+    }
+});
+
+app.get('/create', (req, res) => {
+    res.sendFile('public/createRoom.html', {root: __dirname});
+});
+
+app.get('/join', (req, res) => {
+    res.sendFile('public/joinRoom.html', {root: __dirname});
 });
 
 app.listen(port, () => {
